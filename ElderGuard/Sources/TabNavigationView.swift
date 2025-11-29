3e1b01bcfdfbe39eec45ae3738bee7d1029a7a67
hanhqvu <hanhqvu@users.noreply.github.com>
2025-11-29 15:10:00 +0900

    feat: setup tab navigation

--- ElderGuard/Sources/TabNavigationView.swift
+++ ElderGuard/Sources/TabNavigationView.swift
@@ -1,11 +1,22 @@
 import SwiftUI
 
-struct TabNavigationView: View {
-	var body: some View {
-		TabView {
-			Tab {}
+enum AppTab: String {
+	case home
+	case activity
+}
 
-			Tab {}
+struct TabNavigationView: View {
+	@State private var selectedTab: AppTab = .home
+
+	var body: some View {
+		TabView(selection: $selectedTab) {
+			Tab("Home", systemImage: "house.fill", value: .home) {
+				HomeView()
+			}
+
+			Tab("Activity", systemImage: "list.bullet", value: .activity) {
+				ActivityView()
+			}
 		}
 	}
 }
