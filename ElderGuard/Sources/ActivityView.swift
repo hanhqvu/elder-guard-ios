97b46c0ee73632e9657e0fbfa064cd9bd78e92dd
hanhqvu <hanhqvu@users.noreply.github.com>
2025-11-29 10:41:38 +0900

    feat: tab navigation

---
+++ ElderGuard/Sources/ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  ElderGuard
+//
+//  Created by Hanh Vu on 2025/11/29.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityView()
+}
