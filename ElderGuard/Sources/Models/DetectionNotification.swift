dbb6bf854499f20f7c814b29de3a48098ffbe727
hanhqvu <hanhqvu@users.noreply.github.com>
2025-12-03 05:58:17 +0900

    refactor: detection notification parsing

--- ElderGuard/Sources/Models/DetectionNotification.swift
+++ ElderGuard/Sources/Models/DetectionNotification.swift
@@ -2,9 +2,32 @@ import Foundation
 
 struct DetectionNotification: Codable, Identifiable, Equatable {
 	let id: String
-	let time: Date
+	let time: String
 	let type: String
 	let view: Bool
 
 	var isRead: Bool { view }
+	var timestamp: Date { Self.parseDate(time) }
+}
+
+extension DetectionNotification {
+	private static func parseDate(_ dateString: String) -> Date {
+		var dateString = dateString
+		if !dateString.hasSuffix("Z"), !dateString.contains("+") {
+			dateString += "Z"
+		}
+
+		let iso8601Formatter = ISO8601DateFormatter()
+		iso8601Formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+		if let date = iso8601Formatter.date(from: dateString) {
+			return date
+		}
+		
+		iso8601Formatter.formatOptions = [.withInternetDateTime]
+		return iso8601Formatter.date(from: dateString) ?? Date()
+	}
+
+	private enum CodingKeys: String, CodingKey {
+		case id, time, type, view
+	}
 }
